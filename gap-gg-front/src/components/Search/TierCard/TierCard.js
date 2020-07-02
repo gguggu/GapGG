b2bcdf4574303c3bf0c71ba2fbbd08c0fd9d1b5f
gguggu <adrw1364@naver.co>
2020-07-02 16:27:29 +0900

    [ADD] ADD TierCard
    티어정보 띄우는 카드 생성

---
+++ gap-gg-front/src/components/Search/TierCard/TierCard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './TierCard.scss';
+
+const TierCard = ({ tierData, tierSrc, tierType }) => {
+  const { leaguePoints, losses, rank, tier, wins } = tierData;
+
+  return (
+    <div className="TierCard">
+      <img src={tierSrc}/>
+      <div className="TierCard-contentWrap">
+        <div className="TierCard-contentWrap-queueType">{tierType}</div>
+        <div className="TierCard-contentWrap-tier">{tier} {rank}</div>
+        <div className="TierCard-contentWrap-point">{leaguePoints}LP</div>
+        <div className="TierCard-contentWrap-winlose">{wins}승 {losses}패</div>
+      </div>
+    </div>
+  );
+};
+
+TierCard.propTypes = {
+  tierData: PropTypes.object.isRequired,
+  tierSrc: PropTypes.string.isRequired,
+  tierType: PropTypes.string.isRequired
+};
+
+export default TierCard;
