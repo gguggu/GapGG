c3e0e12a3a902f16ef5cadd805e2bdb041956fac
gguggu <adrw1364@naver.co>
2020-07-27 20:55:33 +0900

    [FIX] FIX HomeContainer
    전적 검색에 필요한 검색 기능 구현

--- gap-gg-front/src/containers/Home/HomeContainer.js
+++ gap-gg-front/src/containers/Home/HomeContainer.js
@@ -11,16 +11,16 @@ const HomeContainer = ({ store, history }) => {
   const { searchSummoner } = store.summoner;
 
   const [name, setName] = useState('');
+  const [subName, setSubName] = useState('');
   const [dbEvent, setDbEvent] = useState(false); // 소환사 이름 중복 이벤트 처리
+  const [searchType, setSearchType] = useState(0);
 
   const requestInitialData = useCallback(async() => {
-    await getChampion().then(async() => {
-      await getRotaition().catch(err => {
-        alert('rotation 에러 발생');
-      });
-    }).catch(err => {
-      alert('champion 에러 발생');
-    });
+    await getChampion()
+    .catch(err => alertError('champion', err));
+
+    await getRotaition()
+    .catch(err => alertError('rotation', err));
 
   }, [getChampion, getRotaition]);
 
@@ -35,17 +35,91 @@ const HomeContainer = ({ store, history }) => {
     setName(value);
   }
 
-  const handleSummonerBtn = async(e) => {
+  const handleSummonerSubName = e => {
+    const { value } = e.target;
+    setSubName(value);
+  }
+
+  const handleSearchType = e => {
+    const { value } = e.target;
+    setSearchType(parseInt(value));
+    resetName();
+  }
+
+  const resetName = () => {
+    setName('');
+    setSubName('');
+  }
+
+  const handleSummonerBtn = async() => {
+    if(name.trim() === ''){
+      alert('이름을 입력해주세요.');
+      return;
+    }
     if(!dbEvent){
       setDbEvent(true);
       const processedName = name.replace(/(\s*)/g, "");
-      await searchSummoner(processedName).then(() => {
+
+      await searchSummoner(processedName, '')
+      .then(() => {
         setDbEvent(false);
         history.push(`/search/${processedName}`);
-      }).catch(err => {
-        alert('이름 에러');
+      })
+      .catch(err => {
+        alertError('summoner', err);
         setDbEvent(false);
       });
+    }
+  }
+
+  const handleSummonerCompareBtn = async() => {
+    if(name.trim() === '' || subName.trim() === ''){
+      alert('이름을 입력해주세요.');
+      return;
+    }
+    if(!dbEvent){
+      setDbEvent(true);
+      const processedName = name.replace(/(\s*)/g, "");
+      const processedSubName = subName.replace(/(\s*)/g, "");
+
+      let isError = false;
+
+      await searchSummoner(processedName, '')
+      .catch(err => {
+        alertError('summoner', err);
+        setDbEvent(false);
+        isError = true;
+      });
+
+      if(isError)
+        return;
+
+      await searchSummoner(processedSubName, 'sub')
+      .then(() => {
+        setDbEvent(false);
+        history.push(`/compare/${processedName}-${processedSubName}`);
+      })
+      .catch(err => {
+        alertError('summoner', err);
+        setDbEvent(false);
+      });
+    }
+  }
+
+  const alertError = (type, err) => {
+    if(type === 'champion'){
+      alert('챔피언 조회 에러');
+      return;
+    }
+
+    if(type === 'summoner'){
+      alert(`소환사 전적 조회 ${err.message}`);
+      return;
+    }
+
+    if(type === 'rotation'){
+      alert(`로테이션 조회 에러  ${err.message}`);
+      return;
     }
   }
 
@@ -56,7 +130,10 @@ const HomeContainer = ({ store, history }) => {
   });
 
   return <Home isPending={isPending} rotationItem={rotationItem} 
-  handleSummonerName={handleSummonerName} handleSummonerBtn={handleSummonerBtn}/>
+  handleSummonerName={handleSummonerName} handleSummonerBtn={handleSummonerBtn}
+  searchType={searchType} handleSearchType={handleSearchType} 
+  handleSummonerCompareBtn={handleSummonerCompareBtn}
+  handleSummonerSubName={handleSummonerSubName}/>;
 };
 
 HomeContainer.propTypes = {
