ecbb4002498c4fb2392fe5e71a0e6126d389ff38
gguggu <adrw1364@naver.co>
2020-07-28 09:48:20 +0900

    [FIX] FIX Navigation
    네비게이션에서 필요하지 않은 Link를 삭제했습니다.

--- gap-gg-front/src/components/common/Navigation/Navigation.js
+++ gap-gg-front/src/components/common/Navigation/Navigation.js
@@ -4,23 +4,17 @@ import { Link } from 'react-router-dom';
 import './Navigation.scss';
 import LOGO from 'assets/images/GapLogo_white.png';
 
-const Navigation = ({ pageType }) => {
+const Navigation = () => {
   return (
     <div className="Navigation">
       <Link className="Navigation-logo" to={'/'}>
         <img src={LOGO} alt="logo"/>
       </Link>
-      <div className="Navigation-menuWrap">
-        <Link to={'/compare'} className={ pageType === 'compare' ? "Navigation-menuWrap-menu same" : "Navigation-menuWrap-menu" }>
-          전적 비교하기
-        </Link>
-      </div>
     </div>
   );
 };
 
 Navigation.propTypes = {
-  pageType: PropTypes.string.isRequired
 };
 
 export default Navigation;
