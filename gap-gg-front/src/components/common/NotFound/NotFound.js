85bacc11df73707e9d6022a90a88b42254b032ca
gguggu <adrw1364@naver.co>
2020-07-28 09:41:56 +0900

    [ADD] ADD NotFound components
    낫파운드 컴포넌트 추가

---
+++ gap-gg-front/src/components/common/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NotFound.scss';
+
+const NotFound = () => {
+  return (
+    <div className="NotFound">
+      <div className="NotFound-title">NOT FOUND | 404</div>
+    </div>
+  );
+};
+
+export default NotFound;
