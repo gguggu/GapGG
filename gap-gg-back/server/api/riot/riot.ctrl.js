e06ff82836d704c09bd7f9227d1f340106d5378d
gguggu <adrw1364@naver.co>
2020-07-28 09:47:01 +0900

    [UPDATE] UPDATE riot.ctrl.js
    서버에서 클라이언트로 response 데이터를 지정해주는 기능을 추가했습니다.

--- gap-gg-back/server/api/riot/riot.ctrl.js
+++ gap-gg-back/server/api/riot/riot.ctrl.js
@@ -6,18 +6,20 @@ const lib = require('../../lib/riotRequest/riotRequest');
 const getChampion = async (req, res) => {
   try {
     const data = await lib.getChampion();
-    res.json(data);
+
+    sendResult(res, data);
   } catch (error) {
-    throw error;
+    catchError(error, res);
   }
 }
 
 const getRotation = async (req, res) => {
   try {
     const data = await lib.getRotation();
-    res.json(data);
+
+    sendResult(res, data);
   } catch (error) {
-    throw error;
+    catchError(error, res);
   }
 }
 
@@ -25,9 +27,10 @@ const searchSummonerName = async (req, res) => {
   try {
     const name = req.query.name;
     const data = await lib.searchSummonerName(name);
-    res.json(data);
+
+    sendResult(res, data);
   } catch (error) {
-    throw error;
+    catchError(error, res);
   }
 }
 
@@ -36,10 +39,12 @@ const searchMatchList = async(req, res) => {
     const accountId = req.query.accountId;
     const begin = parseInt(req.query.beginIndex);
     const end = parseInt(req.query.endIndex);
+    
     const data = await lib.searchMatchList(accountId, begin, end);
-    res.json(data);
+
+    sendResult(res, data);
   } catch (error) {
-    throw error;
+    catchError(error, res);
   }
 }
 
@@ -47,27 +52,30 @@ const searchMatch = async(req, res) => {
   try {
     const matchId = req.query.matchId;
     const data = await lib.searchMatch(matchId);
-    res.json(data);
+
+    sendResult(res, data);
   } catch (error) {
-    throw error;
+    catchError(error, res);
   }
 }
 
 const getQueue = async(req, res) => {
   try {
     const data = await lib.getQueue();
-    res.json(data);
+
+    sendResult(res, data);
   } catch (error) {
-    throw error;
+    catchError(error, res);
   }
 }
 
 const getSpell = async(req, res) => {
   try {
     const data = await lib.getSpell();
-    res.json(data);
+
+    sendResult(res, data);
   } catch (error) {
-    throw error;
+    catchError(error, res);
   }
 }
 
@@ -75,10 +83,38 @@ const getSummonerTier = async(req, res) => {
   try {
     const summonerName = req.query.summonerName;
     const data = await lib.getSummonerTier(summonerName);
-    res.json(data);
+
+    sendResult(res, data);
   } catch (error) {
-    throw error;
+    catchError(error, res);
   }
+}
+
+const sendResult = (res, resultData) => {
+  const result = {
+    status: 200,
+    message: '조회 성공',
+    data: resultData,
+  };
+
+  res.status(200).json(result);
+}
+
+const catchError = (error, res) => {
+  const { statusCode } = error.response;
+  const result = compareErrorStatusMessage(statusCode);
+
+  res.status(result.status).json(result);
+}
+
+const compareErrorStatusMessage = (status) => {
+  if(status === 504) return { status: status, message: '통신 시간제한 초과' };
+
+  if(status === 429) return { status: status, message: 'api 요청 제한 초과' };
+
+  if(status === 404 || status === 400) return { status: status, message: '잘못된 요청 양식' };
+
+  return { status: 500, message: '서버 에러' };
 }
 
 module.exports={
