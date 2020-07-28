ba723fcdeb9af11ff76f84710e3cc2ffefd1485f
gguggu <adrw1364@naver.co>
2020-07-28 09:53:43 +0900

    [FIX] FIX Home
    메인페이지에서 전적검색, 비교 기능을 위한 각 검색 input들을 생성했습니다.

--- gap-gg-front/src/components/Home/Home.js
+++ gap-gg-front/src/components/Home/Home.js
@@ -4,7 +4,8 @@ import HOMELOGO from 'assets/images/GapLogo_white.png';
 import './Home.scss';
 import Loader from 'components/common/Loader';
 
-const Home = ({ isPending, rotationItem, handleSummonerName, handleSummonerBtn }) => {
+const Home = ({ isPending, rotationItem, handleSummonerName, handleSummonerBtn,
+  searchType, handleSearchType, handleSummonerCompareBtn, handleSummonerSubName }) => {
   return (
     <>
     {
@@ -12,13 +13,45 @@ const Home = ({ isPending, rotationItem, handleSummonerName, handleSummonerBtn }
         : 
       <div className="Home">
         <img src={HOMELOGO} alt="homeLogo"/>
-        <label className="Home-searchWrap">
-          <input placeholder="소환사명" onKeyUp={e => {
-            if(e.keyCode === 13)
-              handleSummonerBtn();
-          }} onChange={handleSummonerName}/>
-          <button onClick={handleSummonerBtn}>검색</button>
-        </label>
+        <div className="Home-inputArea">
+          {
+            searchType === 1 ? 
+            <div className="Home-inputArea-compareInput">
+              <label className="Home-inputArea-compareInput-searchWrap">
+                <div className="Home-inputArea-compareInput-searchWrap-inputs">
+                  <input placeholder="비교할 소환사명1" onChange={handleSummonerName} onKeyUp={e => {
+                    if(e.keyCode === 13)
+                      handleSummonerCompareBtn();
+                  }}/>
+                  <input placeholder="비교할 소환사명2" className="bottom" onChange={handleSummonerSubName} onKeyUp={e => {
+                    if(e.keyCode === 13)
+                      handleSummonerCompareBtn();
+                  }}/>
+                </div>
+                <button onClick={handleSummonerCompareBtn}>검색</button>
+              </label>
+            </div>
+              : <label className="Home-inputArea-searchWrap">
+              <input placeholder="소환사명" onKeyUp={e => {
+                if(e.keyCode === 13)
+                  handleSummonerBtn();
+              }} onChange={handleSummonerName}/>
+              <button onClick={handleSummonerBtn}>검색</button>
+            </label>
+          }
+          <div className="Home-inputArea-typeWrap">
+            <label>
+              전적검색
+              <input type="radio" value='0' name="searchType" onChange={handleSearchType} checked={searchType === 0 ? true : false}/>
+              <span></span>
+            </label>
+            <label>
+              전적비교
+              <input type="radio" value='1' name="searchType" onChange={handleSearchType} checked={searchType === 1 ? true : false}/>
+              <span></span>
+            </label>
+          </div>
+        </div>
         <div className="Home-rotationBox">
           <div className="Home-rotationBox-title">오늘의 로테이션</div>
           <div className="Home-rotationBox-items">
@@ -35,7 +68,11 @@ Home.propTypes = {
   isPending: PropTypes.bool.isRequired,
   rotationItem: PropTypes.array.isRequired,
   handleSummonerName: PropTypes.func.isRequired,
-  handleSummonerBtn: PropTypes.func.isRequired
+  handleSummonerBtn: PropTypes.func.isRequired,
+  searchType: PropTypes.number.isRequired,
+  handleSearchType: PropTypes.func.isRequired,
+  handleSummonerCompareBtn: PropTypes.func.isRequired,
+  handleSummonerSubName: PropTypes.func.isRequired
 };
 
 export default Home;
